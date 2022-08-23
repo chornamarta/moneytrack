a7c867ce00287a024b8ed13af8298d4a5d0e1f43
chornamarta <marta.chornaa@gmail.com>
2022-08-23 13:49:08 +0300

    save new expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -8,6 +8,18 @@ class ExpensesController < ApplicationController
   end
 
   def create 
+    @expense = Expense.new(expense_params)
+    # @article.user = current_user
+    if @expense.save
+      # flash[:notice] = "Article was created successfully."
+      redirect_to expenses_path
+    else
+      render 'new'
+    end
+  end
 
+  private
+  def expense_params
+    params.require(:expense).permit(:category,:cost, :date)
   end
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,4 @@
 class Expense < ApplicationRecord
+  validates :category, presence: true, length: {minimum:4, maximum:100}
+  validates :cost, presence: true
 end

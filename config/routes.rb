6ebdbc2868c8969dda8f7c6d6ed93ed97d12af1e
chornamarta <marta.chornaa@gmail.com>
2022-09-07 00:23:27 +0300

    change login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  # root 'pages#home'
   resources :expenses 
   get 'statcategory', to: "expenses#count_cat"
   get 'login', to: "sessions#new"
+  root 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   get 'signup', to: 'users#new'
